cb739ccafcf95135532d83b37633e41dbdb308d4
ycc2001 <ycc2001@gmail.com>
2024-10-24 16:02:00 +0800

    add post method

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+    description: str  = None #None 是預設值
 
 @app.get("/")#定義路徑
-async def root():
+def read_root():
     return {"message": "Hello World"}
 
-@app.get("/lol")#定義路徑
-async def lol():
-    return 'LOOOOL'
+@app.post("/items")
+def create_item(item: Item):
+    print(f"Recevied item: {item}")
+    return {"message": "Item received","item": item}
